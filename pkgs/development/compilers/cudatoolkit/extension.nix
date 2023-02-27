ed143b5c702a2582c9a3d03efbbd214c4a0a9e92
Someone Serge <sergei.kozlukov@aalto.fi>
2023-02-27 16:28:07 +0200

    python3Packages.tensorflow: fix `GLIBCXX_3.4.30' not found
    "Strongly advise" bazel to be consistent about using a cuda-compatible
    toolchain

--- pkgs/development/compilers/cudatoolkit/extension.nix
+++ pkgs/development/compilers/cudatoolkit/extension.nix
@@ -7,11 +7,23 @@ final: prev: let
   # Version info for the classic cudatoolkit packages that contain everything that is in redist.
   cudatoolkitVersions = final.lib.importTOML ./versions.toml;
 
+  finalVersion = cudatoolkitVersions.${final.cudaVersion};
+
+  # Exposed as cudaPackages.backendStdenv.
+  # We don't call it just "stdenv" to avoid confusion: e.g. this toolchain doesn't contain nvcc.
+  # Instead, it's the back-end toolchain for nvcc to use.
+  # We also use this to link a compatible libstdc++ (backendStdenv.cc.cc.lib)
+  backendStdenv = prev.pkgs."${finalVersion.gcc}Stdenv";
+
   ### Add classic cudatoolkit package
-  cudatoolkit = buildCudaToolkitPackage ((attrs: attrs // { gcc = prev.pkgs.${attrs.gcc}; }) cudatoolkitVersions.${final.cudaVersion});
+  cudatoolkit = buildCudaToolkitPackage (finalVersion // { inherit backendStdenv; });
 
-  cudaFlags = final.callPackage ./flags.nix {};
+  cudaFlags = final.callPackage ./flags.nix { };
 
-in {
-  inherit cudatoolkit cudaFlags;
+in
+{
+  inherit
+    backendStdenv
+    cudatoolkit
+    cudaFlags;
 }
