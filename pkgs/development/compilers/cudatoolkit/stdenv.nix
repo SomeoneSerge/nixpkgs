b37f634420d11f0a8aac0e64327f58677eb0864e
Someone Serge <sergei.kozlukov@aalto.fi>
2023-07-12 19:34:50 +0300

    cudaPackages.backendStdenv: explicit -L for libstdc++ and libgcc_s

--- pkgs/development/compilers/cudatoolkit/stdenv.nix
+++ pkgs/development/compilers/cudatoolkit/stdenv.nix
@@ -8,18 +8,18 @@
 }:
 
 let
-  cc = buildPackages.wrapCCWith
-    {
-      cc = ccForStdenv;
+  cc = (buildPackages.wrapCCWith { cc = ccForStdenv; }).overrideAttrs (oldAttrs: {
+    postFixup = (oldAttrs.postFixup or "") + ''
 
-      # This option is for clang's libcxx, but we (ab)use it for gcc's libstdc++.
-      # Note that libstdc++ maintains forward-compatibility: if we load a newer
-      # libstdc++ into the process, we can still use libraries built against an
-      # older libstdc++. This, in practice, means that we should use libstdc++ from
-      # the same stdenv that the rest of nixpkgs uses.
-      # We currently do not try to support anything other than gcc and linux.
-      libcxx = hostLibstdcxxForStdenv;
-    };
+      # When we're taking libstdc++ from a native toolchain
+      echo " -L${hostLibstdcxxForStdenv}/lib" >> $out/nix-support/cc-ldflags
+
+      # Taking target libstdc++ from a cross toolchain
+      if [[ -d ${hostLibstdcxxForStdenv}/${targetPlatform.config}/lib ]] ; then
+        echo " -L${hostLibstdcxxForStdenv}/${targetPlatform.config}/lib" >> $out/nix-support/cc-ldflags
+      fi
+    '';
+  });
   cudaStdenv = stdenvAdapters.overrideCC baseStdenv cc;
   passthruExtra = {
     inherit hostLibstdcxxForStdenv;
