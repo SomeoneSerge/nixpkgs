8ebe5cbb3fb736082e4f34f1cbd1143563f14f09
Someone Serge <sergei.kozlukov@aalto.fi>
2023-09-19 12:28:30 +0300

    nix-required-mounts: init

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -225,6 +225,7 @@
   ./programs/nexttrace.nix
   ./programs/nix-index.nix
   ./programs/nix-ld.nix
+  ./programs/nix-required-mounts.nix
   ./programs/nm-applet.nix
   ./programs/nncp.nix
   ./programs/noisetorch.nix
